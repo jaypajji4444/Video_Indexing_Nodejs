d10dd9781eb6a285c6062fca0dced2f0e1c5d095
jaypajji4444 <43105921+jaypajji4444@users.noreply.github.com>
2020-05-08 20:22:30 +0530

    Add:test for  getting access tokens

---
+++ test/test.js
@@ -0,0 +1,28 @@
+const VideoIndexer=require("../Video_Indexer/index");
+const chai=require("chai")
+
+const expect=chai.expect;
+
+// Credentials
+const subscriptionKey="e71fb83590ea4fe3bbe069a1a7faaada";
+const accountId="9ddc8b28-5fa9-4421-8261-1503f0ee3d24";
+const location ="trial";
+const vi= new VideoIndexer(subscriptionKey,accountId,location)
+// Subsc
+let response;
+describe("Video Indexer",async()=>{
+    describe("(1)AccessToken",async()=>{
+        beforeEach(async()=>{
+        response = await vi.get_access_token()
+        })
+
+        it("status",async(done)=>{
+            expect(response).to.have.property("status").eql(200)
+            done()
+        })
+        it("data",async(done)=>{
+            expect(response).to.have.property("data")
+            done()
+        })
+    })
+})
